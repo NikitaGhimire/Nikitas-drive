24bd945aec549a702b74f5d95fee6131eca32de9
NikitaGhimire <nikitaghimire1@gmail.com>
2024-09-04 12:08:38 +1000

    Project setup achieved with project initialization complete, dependencies(express, prisma, passport, express-session, passport-local, @quixo3/prisma-session-store and multer) installed and basic server setup done

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+
+const app = express();
+
+//Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Session setup
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+//passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+//basic route
+app.get("/", (req, res) => {
+  res.send(`Hello, Welcome to Nikita's Drive!`);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
